00dc0e54ddeb2a2d7422fc155a1816b7e905d5e4
Pol <pbr1111@gmail.com>
2015-09-11 15:34:20 +0200

    Add AutoClicker

---
+++ Modules/AutoClicker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClickerHeroesClicker.Modules
+{
+    public class AutoClicker : Worker
+    {
+        public AutoClicker(IntPtr hwnd)
+        {
+            _hwnd = hwnd;
+            _thread = new Thread(Run);
+        }
+
+        private void Run()
+        {
+            while (true)
+            {
+                wh.WaitOne();
+
+                Shared.SendMouseLeft(_hwnd, Positions.ComboMantainer.X, Positions.ComboMantainer.Y);
+                Shared.SendMouseLeft(_hwnd, Positions.ComboMantainer.X, Positions.ComboMantainer.Y);
+                Shared.SendMouseLeft(_hwnd, Positions.ComboMantainer.X, Positions.ComboMantainer.Y);
+                Shared.SendMouseLeft(_hwnd, Positions.ComboMantainer.X, Positions.ComboMantainer.Y);
+                Thread.Sleep(100);
+            }
+        }
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -27,12 +27,13 @@ namespace ClickerHeroesClicker
                 return;
             }
 
-            bool[] options = { false, false, false };
+            bool[] options = { false, false, false, false };
             ShowOptions(options);
 
             Worker abilitiesThread = new Abilities(hwnd);
             Worker upgradeHeroesThread = new UpgradeHeroes(hwnd);
             Worker clickScreenThread = new ClickClickables(hwnd);
+            Worker autoClickerThread = new AutoClicker(hwnd);
 
             ConsoleKey pressed;
             while ((pressed = Console.ReadKey(true).Key) != ConsoleKey.Escape)
@@ -41,32 +42,34 @@ namespace ClickerHeroesClicker
                 {
                     case ConsoleKey.F1:
                         options[0] = !options[0];
-
                         if (options[0])
                             abilitiesThread.StartOrResume();
                         else
                             abilitiesThread.Pause();
-
                         ShowOptions(options);
                         break;
                     case ConsoleKey.F2:
                         options[1] = !options[1];
-
                         if (options[1])
                             upgradeHeroesThread.StartOrResume();
                         else
                             upgradeHeroesThread.Pause();
-
                         ShowOptions(options);
                         break;
                     case ConsoleKey.F3:
                         options[2] = !options[2];
-
                         if (options[2])
                             clickScreenThread.StartOrResume();
                         else
                             clickScreenThread.Pause();
-
+                        ShowOptions(options);
+                        break;
+                    case ConsoleKey.F4:
+                        options[3] = !options[3];
+                        if (options[3])
+                            autoClickerThread.StartOrResume();
+                        else
+                            autoClickerThread.Pause();
                         ShowOptions(options);
                         break;
                     default:
@@ -78,16 +81,20 @@ namespace ClickerHeroesClicker
             abilitiesThread.Stop();
             upgradeHeroesThread.Stop();
             clickScreenThread.Stop();
+            autoClickerThread.Stop();
 
-            
-            //Scroll
-            /*PressMouseLeft(hwnd, Positions.Scroll.X, Positions.Scroll.DownY);
-            Thread.Sleep(2000);
-            ReleaseMouseLeft(hwnd);
-            // Click 2 cops als monstres cada 5s
-            SendMouseLeft(Positions.ComboMantainer.X, Positions.ComboMantainer.Y);
-            SendMouseLeft(Positions.ComboMantainer.X, Positions.ComboMantainer.Y);*/
+            // Force quit
+            Environment.Exit(Environment.ExitCode);
         }
+
+
+        //Scroll
+        /*PressMouseLeft(hwnd, Positions.Scroll.X, Positions.Scroll.DownY);
+        Thread.Sleep(2000);
+        ReleaseMouseLeft(hwnd);
+        // Click 2 cops als monstres cada 5s
+        SendMouseLeft(Positions.ComboMantainer.X, Positions.ComboMantainer.Y);
+        SendMouseLeft(Positions.ComboMantainer.X, Positions.ComboMantainer.Y);*/
 
         private static void ShowOptions(bool[] options)
         {
@@ -96,6 +103,7 @@ namespace ClickerHeroesClicker
             Console.WriteLine("\tF1 - " + GetTextNextStateOption(options[0]) + " utilitzar habilitats.");
             Console.WriteLine("\tF2 - " + GetTextNextStateOption(options[1]) + " pujar heroi fixe x25.");
             Console.WriteLine("\tF3 - " + GetTextNextStateOption(options[2]) + " clicar als clicables.");
+            Console.WriteLine("\tF4 - " + GetTextNextStateOption(options[3]) + " l'autoclicker.");
             Console.WriteLine("ESC per sortir");
         }
 
